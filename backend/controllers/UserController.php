c66b72258dc2f6f1297255f62c37466b6a2aa2ab
Abner Ivan Angel Camas <satellite15@outlook.es>
2024-04-22 10:55:20 -0600

    terminamos el back para poder hacer login

---
+++ backend/controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+    require_once '../services/UserService.php';
+
+    class UserController{
+        private $userService;
+
+        public function __construct{
+            $db = (new Database())->getConnection();
+            $this->userService = new UserService ($db);
+        }
+
+        public function login(){
+            if($_SERVER["REQUEST_METHOD"]=="POST"){
+                $usuario = $_POST['usuario'];
+                $password = $_POST['password'];
+
+                if(!empty($usuario) && !empty(password)){
+                    $user = $this->userService->login($usuario,$password);
+                    if($user){
+                        //redirigir a otra pagina
+                        echo json_encode(array("success" =>true),
+                        "message" =>"Inicio satisfactorio"); 
+                    }else{
+                        echo json_encode(array("success" =>false),
+                    "message" =>"credenciales incorrectas");
+                    }
+                }else{
+                    echo json_encode(array("success" =>false),
+                    "message" =>"faltan datos");
+                }
+            }else{
+                echo json_encode(array("success" =>false),
+                    "message" =>"Tipo de peticion incorrecta");
+            }
+        }
+    }
+?>
